66142b3387a8914dd7ef9296b3b6050f9affd2fb
Krishnamoorthy K <Krishnamoorthy.K@novigo.com>
2025-01-31 11:40:26 +0530

    checking fix

--- gitpush.bat
+++ gitpush.bat
@@ -1,4 +1,9 @@
 @echo off
+call npm run build
 git add .
-git commit -m "updated the file"
+ 
+:: Prompt the user for a commit message
+set /p commit_msg="Enter commit message: "
+ 
+git commit -m "%commit_msg%"
 git push
