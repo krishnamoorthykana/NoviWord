f3e6145f9bfb0c1170164ad58115ad959405a2f7
Krishnamoorthy K <Krishnamoorthy.K@novigo.com>
2025-01-31 15:02:15 +0530

    testingone

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-undef */
 Office.onReady(function (info) {
+  console.log("Testing outside****");
   if (info.host === Office.HostType.Word) {
   let directLine1=null;
   let flag = true;
